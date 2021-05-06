04668bfd8955d5bcbcca042941335d5908593427
unknown <you@example.com>
2021-05-06 09:28:30 +0700

    updata_setqueen

--- backtracking/set_queen.cpp
+++ backtracking/set_queen.cpp
@@ -4,32 +4,37 @@ using namespace std;
 
 int number_queen;
 int** chessboad = NULL;
+bool* check = NULL;
+int cOunt = 0;
 
 void initData();
 void Cout();
-bool check_set_queen(int row,int col);
+bool check_set_queen(int row, int col);
 void setQueen(int k);
 
 int main()
 {
 	cin >> number_queen;
+	check = new bool[number_queen];
 
 	chessboad = new int* [number_queen];
 	for (size_t i = 0; i < number_queen; i++)
 	{
+		check[i] = true;
 		chessboad[i] = new int[number_queen];
 	}
 
 	initData();
 	setQueen(0);
+	cout << cOunt;
 	return 0;
 }
 
 void initData()
 {
-	for (size_t i = 0; i <number_queen; i++)
+	for (size_t i = 0; i < number_queen; i++)
 	{
-		for (size_t j = 0; j <number_queen ; j++)
+		for (size_t j = 0; j < number_queen; j++)
 		{
 			chessboad[i][j] = 0;
 		}
@@ -51,21 +56,9 @@ void Cout()
 
 bool check_set_queen(int row, int col)
 {
-	// check row
-	for (size_t i = 0; i <col ; i++)
-	{
-		if (chessboad[row][i] == 1)
-			return false;
-	}
-	//check col
-	for (size_t i = 0; i < row; i++)
-	{
-		if (chessboad[i][col] == 1)
-			return false;
-	}
 	//check diagonal top
 	int count1 = 0;
-	while (col-count1>=0&&row-count1>=0)
+	while (col - count1 >= 0 && row - count1 >= 0)
 	{
 		if (chessboad[row - count1][col - count1] == 1)
 			return false;
@@ -73,9 +66,9 @@ bool check_set_queen(int row, int col)
 	}
 	//check diagonal bottom
 	int count2 = 0;
-	while (col-count2>=0&&row+count2<number_queen)
+	while (col - count2 >= 0 && row + count2 < number_queen)
 	{
-		if (chessboad[col - count2][row + count2] == 1)
+		if (chessboad[row + count2][col - count2] == 1)
 			return false;
 		count2++;
 	}
@@ -88,16 +81,25 @@ void setQueen(int k)
 {
 	for (size_t i = 0; i < number_queen; i++)
 	{
-		if (check_set_queen(i, k) == true)
+		if (check[i] == true)
 		{
-			chessboad[i][k] = 1;
+			if (check_set_queen(i, k) == true)
+			{
+				check[i] = false;
 
-			if (k == number_queen - 1)
-				Cout();
-			else
-				setQueen(k + 1);
+				chessboad[i][k] = 1;
 
+				if (k == number_queen - 1)
+				{
+					Cout();
+					cOunt++;
+				}
+				else
+					setQueen(k + 1);
+
+				chessboad[i][k] = 0;
+			}
+			check[i] = true;
 		}
-		chessboad[i][k] = 0;
 	}
 }
